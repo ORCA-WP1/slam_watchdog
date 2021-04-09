5dbf5b4235d84fe3dc8c12cdcdf233aef22fbc9f
Sean Katagiri <sk2023@hw.ac.uk>
2021-04-09 09:47:54 +0100

    edit service names

--- scripts/watchdog.py
+++ scripts/watchdog.py
@@ -13,23 +13,29 @@ from geometry_msgs.msg import PoseArray, Pose, Transform
 
 class Watchdog():
     def __init__ (self):
-        self.stopped_tracking_service = rospy.Service('orbslam_alarm_watchdog_lost_tracking', vehicle_interface.srv.AlarmStoppedTracking, self.handle_AlarmStoppedTracking)
-        self.map_merged = rospy.Service('orbslam_map_merged', vehicle_interface.srv.MapMergingInfo, self.handle_MapMergingInfo)
-        rospy.wait_for_service('planner_reloc_request')
-        self.caller_reloc_request = rospy.ServiceProxy('planner_reloc_request', vehicle_interface.srv.PlannerRelocRequest)
-        self.caller_reloc_done = rospy.ServiceProxy('planner_reloc_done', vehicle_interface.srv.PlannerRelocDone)
-
-        self.odom_sub=rospy.Subscriber("/map_info", MapInfo, self.map_info_callback)
+        rospy.loginfo("starting init")
+        
 
         self.alarms=[]
         self.current_map=0
         self.desired_merges=[]
+        self.map_merged = rospy.Service('orbslam_map_merged', vehicle_interface.srv.MapMergingInfo, self.handle_MapMergingInfo)
+        rospy.wait_for_service('/bluerov2_rosplan/failures/planner_reloc_request')
+        self.caller_reloc_request = rospy.ServiceProxy('/bluerov2_rosplan/failures/planner_reloc_request', vehicle_interface.srv.PlannerRelocRequest)
+        self.caller_reloc_done = rospy.ServiceProxy('/bluerov2_rosplan/failures/planner_reloc_done', vehicle_interface.srv.PlannerRelocDone)
+        self.stopped_tracking_service = rospy.Service("/ORBSLAM3/lost", vehicle_interface.srv.AlarmStoppedTracking, self.handle_AlarmStoppedTracking)
+        rospy.loginfo("alarm stopped tracking s ervice started")
+        self.odom_sub=rospy.Subscriber("/ORBSLAM3/map_info", MapInfo, self.map_info_callback)
+
+        rospy.loginfo("watchdog initialised")
 
 
     def map_info_callback(self,msg):
         self.current_map=msg.current_map_id
 
     def handle_AlarmStoppedTracking(self,request):
+        rospy.loginfo("received call")
+
         #########################################################
         #####  https://www.youtube.com/watch?v=suY06PVK_bI  #####
         #########################################################
@@ -43,15 +49,23 @@ class Watchdog():
                 score=request.keyframe_value[itr]
 
 
-        desired_pose=request.keyframe_value[score_id]
+        # desired_pose=request.keyframe_value[score_id]
+        # reloc_transforms=[]
+        # reloc_transforms.append(request.data[score_id])
+        
+        desired_pose=request.keyframe_value[0]
         reloc_transforms=[]
-        reloc_transforms.append(request.data[score_id])
+        reloc_transforms.append(request.data[0])
         
         #SEND MESSAGE TO PLANNER
-        resp = self.caller_reloc_request(reloc_transforms)
-
-
-        return vehicle_interface.srv.AlarmStoppedTrackingResponse()
+        rospy.loginfo("sending transforms to planner")
+        try:
+            resp = self.caller_reloc_request(reloc_transforms)
+        except Exception() as e:
+            rospy.loginfo(e)
+        rospy.loginfo("=====================================================================")
+        return resp
+        # return vehicle_interface.srv.AlarmStoppedTrackingResponse()
 
     def handle_MapMergingInfo(self,request):
         #Go over alarms and reassign IDs (remove these alarms?)
